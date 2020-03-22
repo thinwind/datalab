c3813da4f87e85b0627a02981d384129ea64dcf6
Shang <niceshang@163.com>
2020-03-23 01:27:32 +0800

    rename db tables and init db

--- assemblyline/models.py
+++ assemblyline/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from . import components
 
 class Param:
-    def __init__(self,key,source=None,title='参数',generator=generators.identity_text_generator):
+    def __init__(self,key,source=None,title='参数',generator=components.identity_text_generator):
         self.key = key
         self.source = source
         self.title = title
@@ -11,30 +11,64 @@ class Param:
 
 
 class Machine(models.Model):
+    class Meta:
+        db_table='al_machine'
     name = models.CharField(max_length=200,blank=False,default="新生产机")
     input_params = models.TextField(blank=True)
     engine = models.CharField(max_length=100,blank=False)
     output_params = models.TextField(blank=True)
     description = models.TextField(blank=True,default="新生产机")
+    status = models.IntegerField(default=1)
     owner = models.ForeignKey('auth.User',related_name='machies',on_delete=models.DO_NOTHING)
-    created = models.DateTimeField(auto_now=False,auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True,auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+class CompoundMachine(models.Model):
+    class Meta:
+        db_table='al_compound_machine'
+    name = models.CharField(max_length=200,blank=False,default="新复合生产机")
+    input_params = models.TextField(blank=True)
+    output_params = models.TextField(blank=True)
+    description = models.TextField(blank=True,default="新复合生产机")
+    sub_machines = models.ManyToManyField(Machine,related_name='compound_machies',through='MachineGroup')
+    status = models.IntegerField(default=1)
+    owner = models.ForeignKey('auth.User',related_name='compound_machies',on_delete=models.DO_NOTHING)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+class MachineGroup(models.Model):
+    class Meta:
+        db_table='al_machine_group'
+    compound_machine = models.ForeignKey(CompoundMachine,on_delete=models.CASCADE)
+    sub_machine = models.ForeignKey(Machine,on_delete=models.CASCADE)
+    postion = models.IntegerField()
 
 class AssemblyLine(models.Model):
+    class Meta:
+        db_table='al_assembly_line'
     name = models.CharField(max_length=200,blank=False,default='新生产线')
     imput_params = models.TextField(blank=True)
     output_params = models.TextField(blank=True)
     description = models.TextField(blank=True,default="新生产线")
-    sub_assemblylines = models.ManyToManyField('self',symmetrical=False,through='SubAssemblyLine',
-            through_fields=('assemblyline','sub_line'))
-    machines = models.ManyToManyField(Machine,through='MachineGroup',through_fields=('assemblyline','machine'))
+    machines = models.ManyToManyField(Machine,through='AssemblyLineMachine',
+            through_fields=('assembly_line','machine'),related_name='assembly_lines')
+    compound_machines = models.ManyToManyField(CompoundMachine,through='AssemblyLineCompoundMachine',
+            through_fields=('assembly_line','compound_machine'),related_name='assembly_lines')
+    status = models.IntegerField(default=1)
+    owner = models.ForeignKey('auth.User',related_name='assembly_lines',on_delete=models.DO_NOTHING)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
-class SubAssemblyLine(models.Model):
-    assemblyline = models.ForeignKey(AssemblyLine,on_delete=models.CASCADE)
-    sub_line = models.ForeignKey(AssemblyLine,on_delete=models.CASCADE)
-    postion = models.IntegerField()
-
-class MachineGroup(models.Model):
-    assemblyline = models.ForeignKey(AssemblyLine,on_delete=models.CASCADE)
+class AssemblyLineMachine(models.Model):
+    class Meta:
+        db_table='al_assembly_line_machine'
+    assembly_line = models.ForeignKey(AssemblyLine,on_delete=models.CASCADE)
     machine = models.ForeignKey(Machine,on_delete=models.CASCADE)
-    postion = models.IntegerField()
+    postion = models.PositiveSmallIntegerField()
+
+class AssemblyLineCompoundMachine(models.Model):
+    class Meta:
+        db_table='al_assembly_line_compound_machine'
+    assembly_line = models.ForeignKey(AssemblyLine,on_delete=models.CASCADE)
+    compound_machine = models.ForeignKey(CompoundMachine,on_delete=models.CASCADE)
+    postion = models.PositiveSmallIntegerField()
